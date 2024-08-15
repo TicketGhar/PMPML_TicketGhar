2281faea0887deb8eb128f4b65aaadcd40d72606
TEJAS HEMANTRAO PATIL <tejaspatil9637@gmail.com>
2024-08-16 03:59:10 +0530

    SMScode for PRN

---
+++ TempProj/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="YourNamespace._Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Send SMS with Twilio</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="SendSmsButton" runat="server" Text="Send SMS" OnClick="SendSmsButton_Click" Height="38px" Width="99px" />
+        </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ TempProj/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web.UI;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace YourNamespace
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void SendSmsButton_Click(object sender, EventArgs e)
+        {
+            // Ensure TLS 1.2 is used
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            string accountSid = System.Configuration.ConfigurationManager.AppSettings["TwilioAccountSid"];
+            string authToken = System.Configuration.ConfigurationManager.AppSettings["TwilioAuthToken"];
+            string twilioPhoneNumber = System.Configuration.ConfigurationManager.AppSettings["TwilioPhoneNumber"];
+            string toPhoneNumber = "+918446415121"; // Replace with the recipient's phone number
+
+            // Generate a 5-digit PNR number
+            string pnrNumber = GeneratePnrNumber();
+
+            string messageBody = $"Welcome to PMPML TicketGhar Your PNR number is: {pnrNumber}";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                to: new PhoneNumber(toPhoneNumber),
+                from: new PhoneNumber(twilioPhoneNumber),
+                body: messageBody
+            );
+
+            Response.Write($"Message sent: {message.Sid}");
+        }
+
+        private string GeneratePnrNumber()
+        {
+            Random random = new Random();
+            int pnr = random.Next(10000, 100000); // Generates a number between 10000 and 99999
+            return pnr.ToString();
+        }
+    }
+}
